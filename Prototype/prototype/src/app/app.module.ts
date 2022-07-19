b830a4cf709814c9c1be8e9e352a48886c131ce4
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2022-07-19 15:52:50 +0200

    #17 added three/soma and orbit controls to 3d view

--- Prototype/prototype/src/app/app.module.ts
+++ Prototype/prototype/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgtCanvasModule} from "@angular-three/core";
-import {NgtAmbientLightModule} from "@angular-three/core/lights";
+import {NgtAmbientLightModule, NgtPointLightModule} from "@angular-three/core/lights";
 import { CubeComponent } from './components/cube/cube.component';
 import {NgtMeshModule} from "@angular-three/core/meshes";
 import {NgtBoxGeometryModule} from "@angular-three/core/geometries";
 import {NgtMeshStandardMaterialModule} from "@angular-three/core/materials";
+import {NgtSobaOrbitControlsModule} from "@angular-three/soba/controls";
 
 
 @NgModule({
@@ -24,6 +25,8 @@ import {NgtMeshStandardMaterialModule} from "@angular-three/core/materials";
     NgtMeshModule,
     NgtBoxGeometryModule,
     NgtMeshStandardMaterialModule,
+    NgtSobaOrbitControlsModule,
+    NgtPointLightModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
