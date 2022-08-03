34546f252739e985f922101a4164d2611b4e56ee
IT180205 <56590851+IT180205@users.noreply.github.com>
2022-08-03 15:54:33 +0200

    Loading GLTF Files in Angular Three,js

--- Prototype/prototype/src/app/app.module.ts
+++ Prototype/prototype/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgtCanvasModule} from "@angular-three/core";
 import {NgtAmbientLightModule, NgtPointLightModule} from "@angular-three/core/lights";
-import { CubeComponent } from './components/cube/cube.component';
+
 import {NgtMeshModule} from "@angular-three/core/meshes";
 import {NgtBoxGeometryModule} from "@angular-three/core/geometries";
 import {NgtMeshStandardMaterialModule} from "@angular-three/core/materials";
 import {NgtSobaOrbitControlsModule} from "@angular-three/soba/controls";
 
+import { Cube2Component } from './cube2/cube2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CubeComponent
+    Cube2Component
   ],
   imports: [
     BrowserModule,
