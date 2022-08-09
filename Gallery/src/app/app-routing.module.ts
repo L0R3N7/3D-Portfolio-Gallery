68cc145bdf26c429d75db0b0a380df531f93334b
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2022-08-09 12:40:20 +0200

    worked on create exhibition page
    added a home link to hidden navbar mode
    added routing to page

--- Gallery/src/app/app-routing.module.ts
+++ Gallery/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {HomePageComponent} from "./site-components/home-page/home-page.component
 import {LogSigninPageComponent} from "./site-components/log-signin-page/log-signin-page.component";
 import {SearchPageComponent} from "./site-components/search-page/search-page.component";
 import {ProfilePageComponent} from "./site-components/profile-page/profile-page.component";
+import {CreateExhibitionPageComponent} from "./site-components/create-exhibition-page/create-exhibition-page.component";
 
 const routes: Routes = [
   {path: '', component:HomePageComponent},
   {path: 'home', component:HomePageComponent},
   {path: 'log-signin', component:LogSigninPageComponent},
   {path: 'search', component:SearchPageComponent},
-  {path: 'profile', component:ProfilePageComponent}
+  {path: 'profile', component:ProfilePageComponent},
+  {path: 'create', component:CreateExhibitionPageComponent}
 
 ]; // sets up routes constant where routes are defined
 
