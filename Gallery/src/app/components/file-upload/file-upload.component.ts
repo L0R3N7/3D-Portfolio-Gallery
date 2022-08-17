cccdfa7113fd96d4b037ef3cc5fb0b0843a3e06d
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2022-08-17 15:57:00 +0200

    worked  on file upload

--- Gallery/src/app/components/file-upload/file-upload.component.ts
+++ Gallery/src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-file-upload',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
+  @Input() media : string = '';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
 }
+
