37d1c20b4ce13c38e786a01baf7c676846e8ee88
emaemaxd <baumname@hotmail.com>
2022-08-23 15:37:47 +0200

    changes, now server not working lol

---
+++ Server/three-d-portfolio-server/src/main/java/org/threeDPortfolioGallery/models/UserDTO.java
@@ -0,0 +1,5 @@
+package org.threeDPortfolioGallery.models;
+
+public class UserDTO {
+
+}
---
+++ Server/three-d-portfolio-server/src/main/java/org/threeDPortfolioGallery/workloads/User.java
@@ -0,0 +1,22 @@
+package org.threeDPortfolioGallery.workloads;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String iconUrl;
+
+   // private
+
+}
