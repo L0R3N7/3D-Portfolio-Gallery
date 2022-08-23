68fd47a7a37477341ac1a4c451602a1c6ad19f83
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2022-08-23 13:59:45 +0200

    working on validation for upload

--- Gallery/src/app/components/exhibition-card/exhibition-card.component.ts
+++ Gallery/src/app/components/exhibition-card/exhibition-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-exhibition-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exhibition-card.component.scss']
 })
 export class ExhibitionCardComponent implements OnInit {
+
+
 
   constructor() { }
 
