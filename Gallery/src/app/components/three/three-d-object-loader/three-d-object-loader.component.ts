ce8ee80b3489a4ec269a032ad5f4c6b86a19bb45
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2022-09-05 15:38:04 +0200

    Roomselection: working on 3d room load

--- Gallery/src/app/components/three/three-d-object-loader/three-d-object-loader.component.ts
+++ Gallery/src/app/components/three/three-d-object-loader/three-d-object-loader.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, inject, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {NgtLoader, NgtObjectMap} from "@angular-three/core";
 import {GLTF, GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {Observable} from "rxjs";
+import loader from "@angular-devkit/build-angular/src/webpack/plugins/single-test-transform";
 
 @Component({
   selector: 'app-three-d-object-loader',
@@ -10,23 +11,43 @@ import {Observable} from "rxjs";
 
 })
 
-export class ThreeDObjectLoaderComponent implements OnInit {
+export class ThreeDObjectLoaderComponent implements OnChanges, OnInit {
   @Input('src') source?: string;
-  model$ : Observable<GLTF & NgtObjectMap> | undefined
   scale : number = 1;
+  model$ : Observable<GLTF & NgtObjectMap> | undefined;
+  first : boolean = true;
+  loader: NgtLoader | undefined;
 
-  constructor(private loader: NgtLoader) {
-
+  constructor() {
   }
 
-  ngOnInit(): void {
-    console.log(this.source)
-    if (this.source){
-      this.model$ = this.loader.use(GLTFLoader, this.source);;
+  ngOnChanges(changes:SimpleChanges){
+    //@ts-ignore
+    if (this.source && changes.source && changes.source.currentValue != changes.source.previousValue) {
+      if (this.first){
+        this.first = false;
+        return;
+      }
+      this.model$ = undefined;
+      this.ngOnInit();
+    }
+  }
+
+  ngOnInit() {
+    this.loader = inject(NgtLoader);
+    this.load3dModel();
+  }
+
+  load3dModel(){
+    console.log(this.source);
+    if (this.source && this.loader) {
+      // @ts-ignore
+      this.model$ = loader.use(GLTFLoader, this.source);
       //@ts-ignore
-      this.model$.subscribe((e)=>{
+      this.model$.subscribe((e) => {
+        console.log(e.scene.children)
         //@ts-ignore
-        this.scale = 2 / e.scene.children[0].geometry.boundingSphere.radius;
+        this.scale = 2 / e.scene.children[0]?.geometry?.boundingSphere?.radius ?? 1;
       })
     }
   }
--- Gallery/src/app/site-components/create-exhibition-page/create-exhibition-roomselection/create-exhibition-roomselection.component.ts
+++ Gallery/src/app/site-components/create-exhibition-page/create-exhibition-roomselection/create-exhibition-roomselection.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {GalleryService} from "../../../shared/gallery.service";
 import {Room} from "../../../shared/room";
 
@@ -11,16 +11,23 @@ export class CreateExhibitionRoomselectionComponent implements OnInit {
   rooms: Room[] = [];
   selectedRoom_id : number = -1;
   room3dUrl: string = "";
+  @ViewChild("resetButton") button : ElementRef | undefined;
 
   @Output() roomSelectedEvent = new EventEmitter<number>();
 
   constructor(public gs: GalleryService) { }
 
   selectRoom(selectedId:number){
+    this.button?.nativeElement.click;
     this.selectedRoom_id = selectedId;
-    this.room3dUrl = this.rooms[selectedId-1].room_url;
     this.roomSelectedEvent.emit(selectedId);
+    // @ts-ignore
+    setTimeout(this.setRoomName(), 500);
     console.log(this.room3dUrl)
+  }
+
+  setRoomName(){
+    this.room3dUrl = this.rooms[this.selectedRoom_id-1].room_url;
   }
 
   ngOnInit(): void {
