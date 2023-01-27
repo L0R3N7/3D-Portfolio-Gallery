8180a63fdd98ad07384ce844b2778acff8b35a0d
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2023-01-27 21:50:26 +0100

    exhibit selection ui change

--- Gallery/src/app/components/exhibit-card/exhibit-card.component.ts
+++ Gallery/src/app/components/exhibit-card/exhibit-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Exhibit} from "../../shared/class/exhibit";
+import {GalleryService} from "../../shared/gallery.service";
 
 @Component({
   selector: 'app-exhibit-card',
@@ -8,16 +9,35 @@ import {Exhibit} from "../../shared/class/exhibit";
 })
 export class ExhibitCardComponent implements OnInit {
 
-  @Input('exhibit') exhibit : Exhibit = new Exhibit(0, "", "", "statue", "");
+  @Input('exhibit') exhibit ?: Exhibit;
   @Input('count') count : number = 0;
   @Output('delete') deleteEvent = new EventEmitter<number>();
+  imageName = ''
 
-  constructor() { }
+  constructor(private galleryService: GalleryService) {
+  }
 
   ngOnInit(): void {
+    this.imageName = this.getImageName()
   }
 
   delete() {
     this.deleteEvent.emit(this.count);
+  }
+
+  getImageName() : string{
+    console.log(this.exhibit)
+
+    if (this.exhibit){
+      const fileTypes = [...this.galleryService.supportedFiletypes.keys()]
+      const fileTypeOfExhibit = fileTypes.find(value => {return this.galleryService.supportedFiletypes.get(value)!.includes(this.exhibit?.data_type ?? '')}) ?? ''
+
+      console.log(this.exhibit.data_type)
+      console.log(this.galleryService.supportedFiletypes)
+      console.log(fileTypeOfExhibit)
+
+      return fileTypeOfExhibit.concat("icon.svg")
+    }
+    return ''
   }
 }
