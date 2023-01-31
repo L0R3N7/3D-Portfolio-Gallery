17a6d19a590477e1214c6dadf1116eebafcb77c6
Lorenz Litzlbauer <lorenz.litzlbauer@gmx.at>
2023-01-31 20:50:33 +0100

    Update user.ts

--- Gallery/src/app/shared/class/user.ts
+++ Gallery/src/app/shared/class/user.ts
@@ -1,14 +1,14 @@
 export class User{
   id: number;
   email: string;
-  usesrname: string;
+  user_name: string;
   password: string;
   icon_url: string;
 
-  constructor(id: number, email: string, usesrname: string, password: string, icon_url: string) {
+  constructor(id: number, email: string, user_name: string, password: string, icon_url: string) {
     this.id = id;
     this.email = email;
-    this.usesrname = usesrname;
+    this.user_name = user_name;
     this.password = password;
     this.icon_url = icon_url;
   }
